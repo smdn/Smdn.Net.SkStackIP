e59b9a46c418462ec9ff19149e6b1e98878e0078
smdn <smdn@smdn.jp>
2021-07-17 18:54:34 +0900

    add example logging

---
+++ examples/logging/Program.cs
@@ -0,0 +1,28 @@
+﻿// SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+
+using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+using Smdn.Net.SkStackIP;
+
+var services = new ServiceCollection();
+
+services.AddLogging(
+  builder => builder
+    .AddSimpleConsole(static options => options.SingleLine = true)
+    .AddFilter(static level => level == LogLevel.Trace)
+    //.AddFilter(static _ => true)
+);
+
+using var client = SkStackClient.Create(
+  serialPortName: "/dev/ttyACM0",
+  serviceProvider:  services.BuildServiceProvider()
+);
+
+await client.SendSKVERAsync();
+await client.SendSKAPPVERAsync();
+await client.SendSKINFOAsync();
