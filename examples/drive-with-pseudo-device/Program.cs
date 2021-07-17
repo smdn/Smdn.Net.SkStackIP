7ec0b43b373dfd37b1f09eefee97b0ab9058f2b4
smdn <smdn@smdn.jp>
2021-07-17 18:48:18 +0900

    add example drive-with-pseudo-device

---
+++ examples/drive-with-pseudo-device/Program.cs
@@ -0,0 +1,35 @@
+﻿// SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+
+using System;
+using System.IO;
+using System.Text;
+
+using Smdn.Net.SkStackIP;
+
+using var pseudoDeviceStream = new PseudoSkStackDeviceStream();
+
+var pseudoDeviceResponseWriter = new StreamWriter(pseudoDeviceStream.GetWriterStream(), Encoding.ASCII);
+
+pseudoDeviceResponseWriter.NewLine = "\r\n";
+pseudoDeviceResponseWriter.AutoFlush = true;
+
+using var client = SkStackClient.Create(stream: pseudoDeviceStream);
+
+// write response lines of SKVER command
+pseudoDeviceResponseWriter.WriteLine("EVER 1.2.3");
+pseudoDeviceResponseWriter.WriteLine("OK");
+
+// send SKVER command
+var respSKVER = await client.SendSKVERAsync();
+
+Console.WriteLine($"VER: {respSKVER.Payload}");
+
+// write response lines of SKAPPVER command
+pseudoDeviceResponseWriter.WriteLine("EAPPVER pseudo-device");
+pseudoDeviceResponseWriter.WriteLine("OK");
+
+// send SKAPPVER command
+var respSKAPPVER = await client.SendSKAPPVERAsync();
+
+Console.WriteLine($"APPVER: {respSKAPPVER.Payload}");
---
+++ examples/drive-with-pseudo-device/PseudoSkStackDeviceStream.cs
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+
+using System;
+using System.IO;
+using System.IO.Pipelines;
+using System.Threading;
+using System.Threading.Tasks;
+
+class PseudoSkStackDeviceStream : Stream {
+  public override bool CanWrite => true;
+  public override bool CanRead => true;
+  public override bool CanSeek => false;
+  public override long Length => throw new NotSupportedException();
+  public override long Position {
+    get => throw new NotSupportedException();
+    set => throw new NotSupportedException();
+  }
+
+  private Pipe readStreamPipe;
+  private Stream readStreamReaderStream;
+  private Stream readStreamWriterStream;
+
+  public PseudoSkStackDeviceStream()
+  {
+    this.readStreamPipe = new Pipe(
+      new PipeOptions(
+        useSynchronizationContext: false
+      )
+    );
+    this.readStreamReaderStream = readStreamPipe.Reader.AsStream();
+    this.readStreamWriterStream = readStreamPipe.Writer.AsStream();
+  }
+
+  public Stream GetWriterStream() => readStreamWriterStream;
+
+  public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
+  public override void SetLength(long value) => throw new NotImplementedException();
+  public override void Flush() => Stream.Null.Flush();
+
+  public override void Write(byte[] buffer, int offset, int count)
+    => Stream.Null.Write(buffer, offset, count);
+
+  public override int Read(byte[] buffer, int offset, int count)
+    => readStreamReaderStream.Read(buffer, offset, count);
+
+  public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    => readStreamReaderStream.ReadAsync(buffer, offset, count, cancellationToken);
+
+  public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+    => readStreamReaderStream.ReadAsync(buffer, cancellationToken);
+}
