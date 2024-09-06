b2472ed5b6a8c80da99ea8f68d86dc68d8e96fe8
smdn <smdn@smdn.jp>
2024-09-06 21:11:29 +0900

    add example for enable logging with BP35A1

---
+++ examples/Smdn.Devices.BP35XX/logging/Program.cs
@@ -0,0 +1,24 @@
+﻿// SPDX-FileCopyrightText: 2024 smdn <smdn@smdn.jp>
+// SPDX-License-Identifier: MIT
+using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Smdn.Devices.BP35XX;
+
+var services = new ServiceCollection();
+
+services.AddLogging(
+  builder => builder
+    .AddSimpleConsole(static options => options.SingleLine = true)
+    .AddFilter(static level => LogLevel.Information <= level)
+);
+
+using var device = await BP35A1.CreateAsync(
+  new BP35A1Configurations() {
+    SerialPortName = "/dev/ttyACM0", // Specify a port name such as COM1 on Windows
+    TryLoadFlashMemory = true, // Try to load configurations stored in flash memory
+  },
+  serviceProvider: services.BuildServiceProvider()
+);
